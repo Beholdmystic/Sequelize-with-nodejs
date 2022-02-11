7cc46b4188ff042640cd3b50dc2206902f359d99
sarin punk <sarin.bhattarai@icp.edu.np>
2022-02-11 16:24:16 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,74 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const errorController = require("./controllers/error");
+const sequelize = require("./util/database");
+const Product = require("./models/product");
+const User = require("./models/user");
+const Cart = require("./models/cart");
+const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+//Relationship between product and user.
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" }); //if user is deleted the product along with them will be deleted = cascade
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, { through: OrderItem });
+
+sequelize
+  // .sync({ force: true }) //not good, force is not used in production but for now to overwrite the data.
+  .sync()
+  .then((result) => {
+    return User.findByPk(1);
+    // console.log(result);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Sar", email: "test@gmail.com" });
+    }
+    return user;
+  })
+  .then((user) => {
+    // console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+//syncs= it syncs your model to the database by creating the appropriate tables
